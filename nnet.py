e29313df2a6e4363df4549fa12e882cf44bf437b
Devin Ward <devinw1325wc@gmail.com>
2017-12-02 18:52:59 -0600

    added lots :)

--- nnet.py
+++ nnet.py
@@ -24,33 +24,43 @@ class NNet():
     def __sigmoid_derivative(self, x):
         return x * (1 - x)
 
-    #Through a process of trial and error, train the net. Adjusts weights each pass.
-    def train(self, training_set_in, training_set_labels, iterations):
-        for iter in xrange(iterations):
+    # Train the net. Adjusts weights each pass.
+    def train(self, training_set_in, training_set_labels, validation_set_in, validation_set_labels, error_target=0.5, max_iterations=100000):
+        iter_count = 0
+        for iter in xrange(max_iterations):
             #pass training into net
             output_of_hidden_layer, output_of_output = self.eval(training_set_in)
+            val_output_of_hidden_layer, val_output_of_output = self.eval(validation_set_in)
 
-            #Back Prop to asses weights.
+            #Back Prop to assess error.
 
             #Calc error for output. 
             #Defined as difference of training labels and predicted output.
             output_error = training_set_labels - output_of_output
             output_delta = output_error * self.__sigmoid_derivative(output_of_output)
+            val_output_error = validation_set_labels - val_output_of_output
 
             #Calc error for hidden layer.
             #Defined as the contribution to the error in output.
             hidden_error = output_delta.dot(self.output_layer.weights.T)
             hidden_delta = hidden_error * self.__sigmoid_derivative(output_of_hidden_layer)
 
-            #Adjust weights according to error found.
+            error_target_check = True if val_output_error < error_target else False
 
-            #calculate adjustments.
-            hidden_adjustment = training_set_in.T.dot(hidden_delta)
-            output_adjustment = output_of_hidden_layer.T.dot(output_delta)
+            iter_count += 1
 
-            #Adjust the weights.
-            self.hidden_layer.weights += hidden_adjustment
-            self.output_layer.weights += output_adjustment
+            #Adjust weights according to error found if error target is not hit.
+            if error_target_check:
+                print 'Within target error at iteration ' + str(iter_count)
+                break
+            else:
+                #calculate adjustments.
+                hidden_adjustment = training_set_in.T.dot(hidden_delta)
+                output_adjustment = output_of_hidden_layer.T.dot(output_delta)
+
+                #Adjust the weights.
+                self.hidden_layer.weights += hidden_adjustment
+                self.output_layer.weights += output_adjustment
 
     #Evaluate the nerual net. 
     def eval(self, inputs):
@@ -64,4 +74,3 @@ class NNet():
         print self.hidden_layer.weights
         print "Output Layer weights: "
         print self.output_layer.weights
-
